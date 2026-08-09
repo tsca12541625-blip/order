fa687d0bdaa37c3fd35ae23e4f8b27cb338c6247
tsca12541625-blip <tsca12541625@gmail.com>
2026-08-10 00:24:01 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUPG7M2twmazWyOWkWlefW05CU-eQhIIM",
+  authDomain: "order-b519b.firebaseapp.com",
+  projectId: "order-b519b",
+  storageBucket: "order-b519b.firebasestorage.app",
+  messagingSenderId: "892773573382",
+  appId: "1:892773573382:web:d5a057754a117ddd567487"
+};
+window.__firebase_config = JSON.stringify(firebaseConfig);
